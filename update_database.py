03568e18c2c3d1f9bd929575b88be323426effa8
santiago_bedoa <stgbedoya@gmail.com>
2021-04-26 17:30:58 -0500

    class UPDATE_DATABASE (will be use to update the data stored in the database) is now ready

--- update_database.py
+++ update_database.py
@@ -5,7 +5,7 @@ import ssl
 class UPDATE_DATABASE:
 
     def __init__(self):
-        self.password = 'Mascota0312'
+        self.password = 'Coronavirus_project_BEDO'
         self.db_name = 'Coronavirus_project_DB'
         self.data = Data()
         self.client = pymongo.MongoClient(
@@ -16,23 +16,71 @@ class UPDATE_DATABASE:
 
 
     def update_global_data(self):
+        '''
+        Updates the "global_data" data that is stored in the database
+        :return:
+        '''
         self.database.global_data.drop()
         collection = self.database['global_data']
         collection.insert_one((self.data.get_actual_global_data()))
 
+        print('Done! global_data updated...')
+
 
     def update_available_countries(self):
+        '''
+        Updates the "available_countries" data that is stored in the database
+        :return:
+        '''
         self.database.available_countries.drop()
         collection = self.database['available_countries']
-        for country in self.data.get_available_countries():
-            collection.insert_one(country)
+        collection.insert_many(self.data.get_available_countries())
+
+        print('Done! available_countries updated...')
 
 
+    def update_countries_actual_data(self):
+        '''
+        Updates the "countries_actual_data" data that is stored in the database
+        :return:
+        '''
+        self.database.countries_actual_data.drop()
+        collection = self.database['countries_actual_data']
+        collection.insert_many(self.data.get_all_countries_actual_data())
+
+        print('Done! countries_actual_data updated...')
 
 
-def main():
-    db = UPDATE_DATABASE()
-    db.update_available_countries()
+    def update_countries_historical_data(self):
+        '''
+        Updates the "acountries_historical_data" data that is stored in the database
+        :return:
+        '''
+        collections = self.database.list_collection_names()
+        available_countries = [country['Slug'] for country in self.database['available_countries'].find()]
+        for count, country in enumerate(available_countries):
+            print(f'{str(count+1)}: {country}')
+            all_data = self.data.get_all_country_data(country)
 
-main()
+            if 0 < len(all_data):
+                collection = self.database[country]
+
+                if country in collections:
+                    last_recorded_date = [x['Date'] for x in self.database[country].find()][-1]
+                    if all_data[-2]['Date'] == last_recorded_date:
+                        last_data = all_data[-1]
+                        collection.insert_one(last_data)
+                    else:
+                        self.database.drop[country]
+                        collection = self.database[country]
+                        collection.insert_many(all_data)
+                else:
+                    self.database.drop[country]
+                    collection = self.database[country]
+                    collection.insert_many(all_data)
+
+            else:
+                print(f'--- No available information for {country} ---')
+
+        print('Done! all available historical data updated...')
 
